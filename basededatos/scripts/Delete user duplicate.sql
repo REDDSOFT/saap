ba817e7eda8d800b5976af4d7046767db7f0737d
Freddy Castillo <freddy.geovanni@gmail.com>
2017-09-07 20:40:02 -0500

    Cargado Proyecto Configurado Correctamente

---
+++ basededatos/scripts/Delete user duplicate.sql
@@ -0,0 +1,10 @@
+﻿delete from   saap.detalle_planilla WHERE id_cabecera_planilla in 
+(
+select cp.id_cabecera_planilla from saap.cabecera_planilla cp inner join saap.llave ll on ( cp.id_llave=ll.id_llave ) inner join saap.usuario us on ( ll.id_usuario=us.id_usuario )
+where 
+cp.id_llave=243
+);
+DELETE FROM saap.lectura WHERE id_llave = 243;
+DELETE FROM saap.cabecera_planilla WHERE id_llave =243;
+DELETE FROM saap.llave WHERE id_llave = 243;
+DELETE FROM saap.usuario WHERE id_usuario = 290;
---
+++ basededatos/scripts/SELECT PANILLAS.sql
@@ -0,0 +1,9 @@
+﻿select *,ll.*,us.* from saap.cabecera_planilla cp inner join saap.llave ll on ( cp.id_llave=ll.id_llave ) inner join saap.usuario us on ( ll.id_usuario=us.id_usuario )
+where 
+--cp.observacion in ('0000000007139','0000000007140')
+--and 
+--us.id_usuario=316
+
+cp.total>100
+
+order  by cp.total
